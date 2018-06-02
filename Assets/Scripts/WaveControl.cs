bdcc872438cadcfb7f0c3e004f8ddb9990ac92c9
Frederik <frederik.wollny@outlook.de>
2018-06-02 13:55:53 +0200

    lalala

---
+++ Assets/Scripts/WaveControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class WaveControl : MonoBehaviour {
+
+    public GameObject enemy;
+    float waitTime = 10.0f;//2Sekunden warten als standart Wert
+    float elapsedTime = 0.0f;
+    string levelName = "";
+    float zeroTime;
+
+
+	// Use this for initialization
+	void Start () {
+        enemy.GetComponent<enemy>();
+        zeroTime = Time.realtimeSinceStartup;
+        
+	}
+    // Update is called once per frame
+    void Update ()
+    {
+        elapsedTime = Time.realtimeSinceStartup;
+        if (elapsedTime >= waitTime)
+        {
+            Instantiate(enemy);
+            waitTime += 10;
+        }
+    }
+}
+
+
---
+++ Assets/Scripts/enemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour {
+
+    public int speed;
+    public Rigidbody en;
+    private Vector3 actualspeed;
+	// Use this for initialization
+
+
+	void Start ()
+    {
+        en.position = new Vector3(0, 20, 0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        actualspeed = new Vector3();
+        en.GetComponent<Rigidbody>();
+        en.transform.TransformPoint(actualspeed);
+        en.velocity = new Vector3(10, actualspeed.y, actualspeed.z);
+    }
+}
