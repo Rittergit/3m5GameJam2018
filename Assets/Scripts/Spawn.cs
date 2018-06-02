71ebd9f5ae94f7a318fa9e05b6f680ecb11cc152
Daniel Martin <daniel.martin@hicknhack-software.com>
2018-06-02 15:18:23 +0200

    * Added simple shroom spawner.

---
+++ Assets/Scripts/Spawn.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject placeableObjectPrefab;
+
+    [SerializeField]
+    private KeyCode newObjectHotkey = KeyCode.B;
+
+    private GameObject currentPlaceableObject;
+    private float mouseWheelRotation;
+
+    private void Update()
+    {
+        HandleNewObjectHotkey();
+        if (currentPlaceableObject!=null)
+        {
+            MoveCurrentPlaceableObjectToMouse();
+            RotateFromMouseWheel();
+            ReleaseIfClicked();
+        }
+    }
+
+    private void ReleaseIfClicked()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            currentPlaceableObject = null;  
+        }
+    }
+
+    private void RotateFromMouseWheel()
+    {
+        mouseWheelRotation = Input.mouseScrollDelta.y;
+        currentPlaceableObject.transform.Rotate(Vector3.up, mouseWheelRotation * 10f);
+
+    }
+
+    private void MoveCurrentPlaceableObjectToMouse()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hitInfo;
+        if(Physics.Raycast(ray,out hitInfo, 1 << LayerMask.NameToLayer("Terrain")))
+        {
+            currentPlaceableObject.transform.position = hitInfo.point;
+            currentPlaceableObject.transform.rotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
+        }
+    }
+
+    private void HandleNewObjectHotkey()
+    {
+        if (Input.GetKeyDown(newObjectHotkey))
+        {
+            if (currentPlaceableObject == null)
+            {
+                currentPlaceableObject = Instantiate(placeableObjectPrefab);
+            }
+            else
+            {
+                Destroy(currentPlaceableObject);
+            }
+        }
+    }
+}
