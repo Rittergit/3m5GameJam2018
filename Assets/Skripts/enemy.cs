57e64e1dd075bc62706d90a419c9e4bb825787ae
Frederik <frederik.wollny@outlook.de>
2018-06-02 11:57:38 +0200

    krasse skripte

---
+++ Assets/Skripts/enemy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour {
+
+    public Rigidbody phys;
+	// Use this for initialization
+	void Start () {
+        phys = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        phys.velocity = new Vector3(1, 0, 0);
+	}
+}
