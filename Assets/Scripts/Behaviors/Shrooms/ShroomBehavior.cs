0781d5bd28d23670d14ad76fe59e76ed8e25a57d
Daniel Martin <daniel.martin@hicknhack-software.com>
2018-06-03 14:19:16 +0200

    * Added basic intro text. * Added text health bar.

--- Assets/Scripts/Behaviors/Shrooms/ShroomBehavior.cs
+++ Assets/Scripts/Behaviors/Shrooms/ShroomBehavior.cs
@@ -3,14 +3,24 @@
 public class ShroomBehavior : MonoBehaviour {
 
 	public Shroom shroom;
+    private float healthBarLength;
 
-	protected virtual void Start () {
-	}
+    protected virtual void Start ()
+    {
+        healthBarLength = Screen.width / 15;
+    }
 
 	protected virtual void Update () {
 		if (this.shroom.currentHealth <= 0) {
 			Destroy (this.gameObject);
+            return;
 		}
-	}
+    }
+    
+    void OnGUI()
+    {
+        var screenPos = Camera.main.WorldToScreenPoint(this.transform.position);
+        GUI.Box(new Rect(screenPos.x - (healthBarLength / 2), Screen.height - screenPos.y, healthBarLength, 20), this.shroom.currentHealth + "/" + this.shroom.maxHealth);
+    }
 
 }
