0a6004fc6b21a630395dd8bba51fcd790338c0e9
Daniel Martin <daniel.martin@hicknhack-software.com>
2018-06-03 15:40:04 +0200

    * Added healthbar coloring. * Added on mouse over health. * Added prothese healthbar.

--- Assets/Scripts/Behaviors/Prothese/ProtheseBehavior.cs
+++ Assets/Scripts/Behaviors/Prothese/ProtheseBehavior.cs
@@ -1,16 +1,46 @@
 ﻿using UnityEngine;
 
-public class ProtheseBehavior : MonoBehaviour {
+public class ProtheseBehavior : MonoBehaviour
+{
 
-	public Prothese prothese;
+    public Prothese prothese;
+    private float healthBarBackgroundLength;
+    private float healthBarForegroundLength;
+    //private GUIStyle healthBarStyle;
 
-	protected virtual void Start () {
-	}
+    protected virtual void Start()
+    {
+        healthBarBackgroundLength = Screen.width / 10;
+        healthBarForegroundLength = healthBarBackgroundLength;
+    }
 
-	protected virtual void Update () {
-		if (this.prothese.currentHealth <= 0) {
-			Destroy (this.gameObject);
-		}
-	}
+    protected virtual void Update()
+    {
+        if (this.prothese.currentHealth <= 0)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        healthBarForegroundLength = healthBarBackgroundLength * (float)((float)this.prothese.currentHealth / (float)this.prothese.maxHealth);
+    }
+
+    protected void OnGUI()
+    {
+        var screenPos = Camera.main.WorldToScreenPoint(this.transform.position);
+        //GUI.Box(new Rect(screenPos.x - (healthBarLength / 2), Screen.height - screenPos.y, healthBarLength, 20), this.shroom.currentHealth + "/" + this.shroom.maxHealth);
+
+        var healthBarStyle = new GUIStyle(GUI.skin.box);
+        healthBarStyle.normal.background = Texture2D.whiteTexture;
+        healthBarStyle.normal.textColor = Color.white;
+        var oldguicolor = GUI.backgroundColor;
+        GUI.backgroundColor = Color.black;
+        GUI.Box(new Rect(screenPos.x - (healthBarBackgroundLength / 2), Screen.height - screenPos.y, healthBarBackgroundLength, 1), "", healthBarStyle);
+        GUI.backgroundColor = Color.green;
+        GUI.Box(new Rect(screenPos.x - (healthBarBackgroundLength / 2), Screen.height - screenPos.y, healthBarForegroundLength, 1), "", healthBarStyle);
+        GUI.backgroundColor = oldguicolor;
+
+
+    }
 
 }
