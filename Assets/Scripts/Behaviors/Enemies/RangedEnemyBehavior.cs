9c7c8a7f8f4f55de883eb26e85e3b421f2427048
Daniel Martin <daniel.martin@hicknhack-software.com>
2018-06-03 15:59:00 +0200

    * Fixed ranged enemy.

--- Assets/Scripts/Behaviors/Enemies/RangedEnemyBehavior.cs
+++ Assets/Scripts/Behaviors/Enemies/RangedEnemyBehavior.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RangedEnemyBehavior : EnemyBehavior {
+public class RangedEnemyBehavior : EnemyBehavior
+{
 
     private new Collider collider;
     private Rigidbody rigidbody;
@@ -23,7 +24,6 @@ public class RangedEnemyBehavior : EnemyBehavior {
     private Transform getTarget()
     {
         float nearestdist = 99999f;
-        //Vector3 nearestpos = new Vector3(0,0,0);
         Transform target = null;
         var shrooms = GameObject.FindGameObjectsWithTag("Shroom");
         int len = shrooms.GetLength(0);
@@ -54,74 +54,29 @@ public class RangedEnemyBehavior : EnemyBehavior {
         }
     }
 
-    private void Move(Transform target)
-    {
-        
-        if (Vector3.Distance(target.position,this.transform.position)<6)
-        {
-            var bulletGameObj = Instantiate(this.enemy.bullet.model, this.transform.position, Quaternion.identity).gameObject;
-            var bulletScript = bulletGameObj.GetComponent(typeof(EnemyBulletBehavior)) as EnemyBulletBehavior;
-            bulletScript.bullet = this.enemy.bullet;
-            bulletScript.target = target;
-        }else
-        {
-            this.transform.position = Vector3.MoveTowards(this.transform.position, target.position, 0.05f);
-        }
-
-        return;
-    }
     protected override void Update()
     {
         base.Update();
 
         var target = getTarget();
-        this.Move(target);
 
-        
-
-        //// Get Objects infront
-        //var centerPos = this.collider.bounds.center;
-        //var fwd = this.transform.forward;
-        //RaycastHit hit;
-        //if (Physics.Raycast (centerPos, fwd, out hit, this.gorilla.bullet.range) && (hit.collider.tag == "Plant")) {
-        //	this.hittedGameObject = hit.collider.gameObject;
-        //}
-
-        //if (this.hittedGameObject == null) {
-        //	// Movement
-        //	if (this.gorilla.movementspeed != 0) {
-        //		this.transform.Translate ((Vector3.forward * Time.deltaTime) * this.gorilla.movementspeed);
-        //	}
-
-        //	// Reset Attack Animation
-        //	this.animator.SetBool ("Attack", false);
-        //}
-
-        //this.attackTimer -= Time.deltaTime;
-        //if (this.attackTimer < 0) {
-        //	// Reset Timer
-        //	this.attackTimer = this.gorilla.attackspeed;
-
-        //	if (this.hittedGameObject == null) {
-        //		return;
-        //	}
-
-        //	// Set Attack Animation
-        //	this.animator.speed = 1 / this.gorilla.attackspeed;
-        //	this.animator.SetBool ("Attack", true);
-
-        //	// Damage Calculation
-        //	var plantScript = this.hittedGameObject.GetComponentInParent(typeof(PlantBehavior)) as PlantBehavior;
-        //	plantScript.plant.health -= this.gorilla.bullet.damage;
-        //}
+        if (Vector3.Distance(target.position, this.transform.position) < 6)
+        {
+            this.attackTimer -= Time.deltaTime;
+            if (this.attackTimer < 0)
+            {
+                // Reset Timer
+                this.attackTimer = this.enemy.attackspeed;
+                var bulletGameObj = Instantiate(this.enemy.bullet.model, this.transform.position, Quaternion.identity).gameObject;
+                var bulletScript = bulletGameObj.GetComponent(typeof(EnemyBulletBehavior)) as EnemyBulletBehavior;
+                bulletScript.bullet = this.enemy.bullet;
+                bulletScript.target = target;
+            }
+        }
+        else
+        {
+            this.transform.position = Vector3.MoveTowards(this.transform.position, target.position, 0.05f);
+        }
     }
-
-    void OnCollisionEnter(Collision col)
-    {
-        var colGameObj = col.gameObject;
-            var shroomScript = colGameObj.GetComponentInParent(typeof(ShroomBehavior)) as ShroomBehavior;
-            shroomScript.shroom.currentHealth -= this.enemy.attack;
-            Destroy(this.gameObject);
-        
-    }
+    
 }
